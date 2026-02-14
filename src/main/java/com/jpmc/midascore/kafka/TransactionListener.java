f2729ef0f16a1ca5dd38937394a8e808d2014738
Haseebm01 <haseebdr01@gmail.com>
2026-02-13 17:00:31 -0800

    modified

--- src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -1,5 +1,7 @@
 package com.jpmc.midascore.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -7,12 +9,14 @@ import com.jpmc.midascore.foundation.Transaction;
 
 @Component
 public class TransactionListener {
-
+    
+    private static final Logger log = LoggerFactory.getLogger(TransactionListener.class);
     private int count = 0;
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
     public void listen(Transaction transaction) {
         count++;
+        log.info("=== RECEIVED TRANSACTION #{} - Amount: {} ===", count, transaction.getAmount());
         if (count <= 4) {
             System.out.println("Transaction #" + count + " Amount: " + transaction.getAmount());
         }
